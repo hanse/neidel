7b1940775a0056b801994549cb357981a5781bd1
Hanse <hanse19@gmail.com>
2015-11-25 14:23:00 +0100

    better fetch abstraction

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -2,25 +2,31 @@ import { Post } from './types';
 
 const url = 'http://localhost:3000/api';
 
-function checkStatus(response) {
-  if (response.status === 200) {
-    return response;
-  }
-  const error = new Error(response.statusText);
-  error.response = response;
-  throw error;
+function fetchJSON(path, options) {
+  return fetch(path, {
+    ...options,
+    headers: {
+      ...options.headers,
+      'Content-Type': 'application/json'
+    }
+  }).then((response) => {
+    if (response.status >= 200 && response.status < 300) {
+      return response;
+    }
+    const error = new Error(response.statusText);
+    error.response = response;
+    throw error;
+  }).then(response => response.json());
 }
 
 export function fetchPosts(path) {
   return (dispatch, getState) => {
     dispatch({ type: Post.FETCH_BEGIN });
-    return fetch(`${url}/posts${path}`, {
+    return fetchJSON(`${url}/posts${path}`, {
       headers: {
         plebtoken: getState().auth.get('token') || ''
       }
-    }).then(checkStatus)
-      .then(res => res.json())
-      .then(
+    }).then(
         result => dispatch({ type: Post.FETCH_SUCCESS, posts: result.posts }),
         error => dispatch({ type: Post.FETCH_FAILURE, error })
       );
@@ -33,17 +39,13 @@ export function savePost(message) {
     if (!message) return;
     dispatch({ type: Post.SAVE_BEGIN, message });
 
-    fetch(`${url}/posts`, {
+    fetchJSON(`${url}/posts`, {
       method: 'post',
       headers: {
         plebtoken: getState().auth.get('token') || '',
-        'Content-Type': 'application/json'
       },
       body: JSON.stringify({ message })
-    })
-      .then(checkStatus)
-      .then(res => res.json())
-      .then(
+    }).then(
         result => (
           dispatch(fetchPosts(getState().router.location.pathname)),
           dispatch({ type: Post.SAVE_SUCCESS, result })
@@ -56,9 +58,7 @@ export function savePost(message) {
 export function upvote(postId) {
   return (dispatch) => {
     dispatch({ type: Post.UPVOTE_BEGIN, postId });
-    return fetch(`${url}/posts/${postId}/upvote`, { method: 'post' })
-      .then(checkStatus)
-      .then(res => res.json())
+    return fetchJSON(`${url}/posts/${postId}/upvote`, { method: 'post' })
       .then(
         () => dispatch({ type: Post.UPVOTE_SUCCESS, postId }),
         error => dispatch({ type: Post.UPVOTE_FAILURE, error })
@@ -69,9 +69,7 @@ export function upvote(postId) {
 export function downvote(postId) {
   return (dispatch) => {
     dispatch({ type: Post.DOWNVOTE_BEGIN, postId });
-    return fetch(`${url}/posts/${postId}/downvote`, { method: 'post' })
-      .then(checkStatus)
-      .then(res => res.json())
+    return fetchJSON(`${url}/posts/${postId}/downvote`, { method: 'post' })
       .then(
         () => dispatch({ type: Post.DOWNVOTE_SUCCESS, postId }),
         error => dispatch({ type: Post.DOWNVOTE_FAILURE, error })
